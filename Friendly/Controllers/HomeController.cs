0e42a31e6dc808c261ee6f035381ed2af7bc9b6b
lpchamarthy <lchamart@hawk.iit.edu>
2017-11-07 12:38:36 -0800

    Version 1

---
+++ Friendly/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friendly.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Home
+        public ActionResult Wall()
+        {
+            return View();
+        }
+        // GET: Home
+        public ActionResult Freinds()
+        {
+            return View();
+        }
+    }
+}
---
+++ Friendly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Friendly.MvcApplication" Language="C#" %>
